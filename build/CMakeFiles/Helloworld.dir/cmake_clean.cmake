8b68bdab561bb2c203ff5bb30235df3bb5ef416e
blinov555 <60940075+blinov555@users.noreply.github.com>
2023-12-09 19:39:36 +0500

    +build

---
+++ build/CMakeFiles/Helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Helloworld.dir/main.cpp.obj"
+  "CMakeFiles/Helloworld.dir/main.cpp.obj.d"
+  "Helloworld.exe"
+  "Helloworld.exe.manifest"
+  "Helloworld.pdb"
+  "libHelloworld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Documents/GitHub/1.Helloworld
+# Build directory: C:/Users/user/Documents/GitHub/1.Helloworld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
